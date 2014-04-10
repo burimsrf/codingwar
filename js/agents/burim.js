70ce913e1f7ea6fa199c1ea0ec2b957cbe45f271
burimsrf <burim.muqa@srf.ch>
2014-04-10 17:01:31 +0200

    Changed the burim agent to move faster

--- js/agents/burim.js
+++ js/agents/burim.js
@@ -7,22 +7,27 @@
         positionY: 0,
         shooting: false,
         shootingX: 0,
+        deltaTime: 0,
+        speed: 100,
+        imgSrc: "images/burim.png",
+        hit: 0,
+        lastHit:false,
         init: function() {
         },
         setKeycode: function(keycode) {
             this.shooting = false;
             switch(keycode) {
-                case 37: 
-                    this.positionX--;
+                case 37:
+                    this.positionX-= this.speed * this.deltaTime;
                     break;
-                case 38: 
-                    this.positionY--;
+                case 38:
+                    this.positionY-= this.speed * this.deltaTime;
                     break;
-                case 39: 
-                    this.positionX++;
+                case 39:
+                    this.positionX+= this.speed * this.deltaTime;
                     break;
                 case 40:
-                    this.positionY++;
+                    this.positionY+= this.speed * this.deltaTime;
                     break;
                 case 32:
                     this.shooting = true;
@@ -34,13 +39,23 @@
             var context = container.getContext('2d');
 
             var imageObj = new Image();
+            var width = 20;
+            var height = 9;
 
-            imageObj.src = 'images/burim.png';
-            context.drawImage(imageObj,this.positionX,this.positionY,20,9);
+            if (this.hit == 1) {
+                width = 40;
+                height = 18;
+            }
+
+            imageObj.src = this.imgSrc;
+            context.drawImage(imageObj,this.positionX,this.positionY,width,height)
+
+            //context.rotate(20*Math.PI/180);
 
             if (this.shooting) {
                 this.shoot(context, this.shootingX, this.shootingY);
                 this.shootingX = this.shootingX + 5;
+                //this.grayScale(context, this.positionX,this.positionY,width,height);
             }
         },
 
@@ -56,6 +71,20 @@
             shootSound.volume = .25;
             shootSound.load();
             shootSound.play();
+        },
+
+        grayScale: function (context, posX, posY, imgWidth, imgHeight) {
+            var imgData = context.getImageData(posX, posY, imgWidth, imgHeight);
+            var pixels  = imgData.data;
+            for (var i = 0, n = pixels.length; i < n; i += 4) {
+                var grayscale = pixels[i] * .3 + pixels[i+1] * .59 + pixels[i+2] * .11;
+                pixels[i  ] = grayscale;        // red
+                pixels[i+1] = grayscale;        // green
+                pixels[i+2] = grayscale;        // blue
+                //pixels[i+3]              is alpha
+            }
+            //redraw the image in black & white
+            context.putImageData(imgData, posX, posY, imgWidth, imgHeight);
         }
     }
 
