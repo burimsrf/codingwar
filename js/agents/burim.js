2130116f86966d569ea7320be79b3e321fd27a3f
burimsrf <burim.muqa@srf.ch>
2014-04-10 14:01:17 +0200

    Changed the burim client

--- js/agents/burim.js
+++ js/agents/burim.js
@@ -2,12 +2,15 @@
     "use strict";
 
     var Rocket = {
-        name: "Pascal",
+        name: "Burim",
         positionX: 0,
         positionY: 0,
+        shooting: false,
+        shootingX: 0,
+        shootingY: 0,
         init: function() {
         },
-        setKeycode: function(keycode) {
+        setKeycode: function(keycode,container) {
             switch(keycode) {
                 case 37: 
                     this.positionX--;
@@ -21,13 +24,32 @@
                 case 40:
                     this.positionY++;
                     break;
+                case 32:
+                    this.shooting = true;
+                    break;
             }
         },
         draw: function(container) {
             var context = container.getContext('2d');
+            //context.fillStyle = "rgb(12,12,12)";
+
+            var imageObj = new Image();
+
+            //imageObj.onload = function() {
+                imageObj.src = 'http://local/codingwar/images/burim.png';
+                context.drawImage(imageObj,this.positionX,this.positionY,20,9);
+            //};
+
+            if (this.shooting) {
+                this.shoot(context, this.shootingX, this.shootingY);
+
+                this.shootingX = this.shootingX + 5;
+            }
+        },
+
+        shoot: function(context, posX, posY) {
             context.fillStyle = "rgb(12,12,12)";
-            
-            context.fillRect(this.positionX, this.positionY, 10, 10);
+            context.fillRect(this.positionX + posX, this.positionY+4, 2, 2);
         }
     }
 
